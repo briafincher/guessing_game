be3bd8d643f8ed6ce9db43edf20e16681b047093
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 15:09:43 -0700

    Catches if user does not input integer

--- game.py
+++ game.py
@@ -7,11 +7,17 @@ name = raw_input('Name: ')
 number = random.randint(1, 100)
 
 num_guesses = 0
+guess = 0
 
 while True:
-
-    guess = int(raw_input('Pick a number between 1 and 100: '))
     num_guesses += 1
+
+    try:
+        guess = int(raw_input('Pick a number between 1 and 100: '))
+    except ValueError:
+        print "Sorry, you must choose an integer!"
+        continue
+
     if guess < 1 or guess > 100:
         print 'That is not a valid number. Enter a number between 1 and 100.'
     elif guess > number:
