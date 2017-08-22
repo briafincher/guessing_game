cce227527c23ff6c6bd0f8232581c3a24e6e0747
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 12:32:59 -0700

    adding guess counter

--- game.py
+++ game.py
@@ -5,14 +5,17 @@ print 'Hello there! What is your name?'
 name = raw_input('Name: ')
 
 number = random.randint(1, 100)
-print number
+
+num_guesses = 0
 
 while True:
+
     guess = int(raw_input('Pick a number between 1 and 100: '))
+    num_guesses += 1
     if guess > number:
         print 'Too high!'
     elif guess < number:
         print 'Too low!'
     else:
-        print 'Great job, {}!'.format(name)
+        print 'Great job, {}! You found my number in {} guesses'.format(name, num_guesses)
         break
