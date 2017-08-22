1c6a2381bf2093534af34bf27c4ea72aaca11655
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 12:27:11 -0700

    Gets user guess, checks if guess is correct

--- game.py
+++ game.py
@@ -5,4 +5,14 @@ print 'Hello there! What is your name?'
 name = raw_input('Name: ')
 
 number = random.randint(1, 100)
+print number
 
+while True:
+    guess = int(raw_input('Pick a number between 1 and 100: '))
+    if guess > number:
+        print 'Too high!'
+    elif guess < number:
+        print 'Too low!'
+    else:
+        print 'Great job, {}!'.format(name)
+        break
